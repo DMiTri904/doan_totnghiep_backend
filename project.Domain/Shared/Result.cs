8d5eb6d8a4c31f38902af6d35e185a303708552a
nnkongh <169603869+DMiTri904@users.noreply.github.com>
2026-03-04 10:45:57 +0700

    Result pattern

---
+++ project.Domain/Shared/Result.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project.Domain.Shared
+{
+    public class Result
+    {
+        protected internal Result(bool isSuccess, Error error)
+        {
+            if (isSuccess && error != Error.None)
+            {
+                throw new InvalidOperationException();
+            }
+            if (!isSuccess && error == Error.None)
+            {
+                throw new InvalidOperationException();
+            }
+            IsSuccess = isSuccess;
+            Error = error;
+        }
+        public bool IsSuccess { get; }
+        public bool IsFailure => !IsSuccess;
+        public Error Error { get; }
+        public static Result Success() => new(true, Error.None);
+        public static Result<TValue> Success<TValue>(TValue value) => new(value, true, Error.None);
+        public static Result Failure(Error error) => new(false, error);
+        public static Result<TValue> Failure<TValue>(Error error) => new(default, false, error);
+        public static Result<TValue> Create<TValue>(TValue? value) => value is not null ? Success(value) : Failure<TValue>(Error.None);
+    }
+    public class Result<T> : Result
+    {
+        private readonly T? _value;
+        protected internal Result(T? value, bool isSuccess, Error error) : base(isSuccess, error)
+        {
+            _value = value;
+        }
+        public T Value => IsSuccess ? _value! : throw new InvalidOperationException("Cannot access Value when Result is failure.");
+        public static implicit operator Result<T>(T value) => Create(value);
+    }
+    public class Error
+    {
+        public static readonly Error None = new(string.Empty, string.Empty);
+        public static readonly Error NullValue = new("Error.NullValue", "The value cannot be null.");
+        public Error(string code, string message)
+        {
+            Code = code;
+            Message = message;
+        }
+        public string Code { get; }
+        public string Message { get; }
+
+        public bool Equals(Error? other)
+        {
+            if (other is null)
+            {
+                return false;
+            }
+            return Code == other.Code && Message == other.Message;
+        }
+    }
+}
