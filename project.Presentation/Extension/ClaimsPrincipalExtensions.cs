463ffabdd405ab29e875a648edfb3b3ba8c1dc13
dangminhtri94@gmail.com <169603869+DMiTri904@users.noreply.github.com>
2026-03-09 13:00:36 +0700

    Get Claim user extension

---
+++ project.Presentation/Extension/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace project.Presentation.Extension
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static int? GetUserId(this ClaimsPrincipal user)
+        {
+            var claim = user.FindFirst(JwtRegisteredClaimNames.Sub)?.Value;
+            if(claim == null) return null;
+            return int.TryParse(claim, out var id) ? id : null;
+        }
+    }
+}
