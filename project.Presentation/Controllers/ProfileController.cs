67098cfbc34970a98c439fe6793ccd78ca235c82
dangminhtri94@gmail.com <169603869+DMiTri904@users.noreply.github.com>
2026-03-09 13:01:07 +0700

    Add view profile controller

---
+++ project.Presentation/Controllers/ProfileController.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using project.Application.Features.Query.Profile;
+using project.Presentation.Extension;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace project.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    public class ProfileController : ApiController
+    {
+        public ProfileController(ISender sender) : base(sender)
+        {
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Profile()
+        {
+            var userId = User.GetUserId();
+            if (userId == null) return Unauthorized();
+            var query = new UserProfileQuery(userId.Value);
+            var result = await _sender.Send(query);
+            return result.IsSuccess ? Ok(result.Value) : NotFound(result.Error);
+        }
+    }
+}
