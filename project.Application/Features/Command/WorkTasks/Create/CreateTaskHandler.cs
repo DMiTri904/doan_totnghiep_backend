5b4eb3e9792aead732f8498b9e2438f58cf4ca44
DMTri904 <dangminhtri94@gmail.com>
2026-06-08 23:32:30 +0700

    Check the validity of duedate

--- project.Application/Features/Command/WorkTasks/Create/CreateTaskHandler.cs
+++ project.Application/Features/Command/WorkTasks/Create/CreateTaskHandler.cs
@@ -44,7 +44,10 @@ namespace project.Application.Features.Command.WorkTasks.Create
         {
             try
             {
-
+                if (request.DueDate.HasValue)
+                {
+                    if (request.DueDate.Value <  DateTime.UtcNow) return Result.Failure<TaskModel>(new Error("404", "Ngày đến hạn phải là ngày trong tương lai"));
+                }
                 var group = await _groupRepository.GetByIdWithMemberAsync(request.GroupId);
                 if (group == null) return Result.Failure<TaskModel>(new Error("404", "Không tìm thấy nhóm"));
                 if (!group.IsActive) return Result.Failure<TaskModel>(new Error("403", "Nhóm đã bị vô hiệu hóa"));
